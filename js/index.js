4dc77a0e8c86f1c74b462764eca67a4c0519fe34
Habib-0007 <adebayohabib7@gmail.com>
2024-06-30 23:22:16 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+var todayText =
+	document.querySelector(".today");
+var utcTime =
+	document.querySelector(".utcDate");
+
+var date = new Date();
+var today = date.getDay();
+
+var text;
+
+switch (today) {
+	case 0:
+		text = "Sunday";
+		break;
+	case 1:
+		text = "Monday";
+		break;
+	case 2:
+		text = "Tuesday";
+		break;
+	case 3:
+		text = "Wednesday";
+		break;
+	case 4:
+		text = "Thursday";
+		break;
+	case 5:
+		text = "Friday";
+		break;
+	case 6:
+		text = "Saturday";
+		break;
+
+	default:
+		text = "Random day 😊";
+}
+
+todayText.textContent = `${text}`;
+
+let timeInMilliseconds = () => {
+	const mainTime = Date.now();
+	utcTime.textContent = `${mainTime}`;
+};
+
+setInterval(timeInMilliseconds, 1);
